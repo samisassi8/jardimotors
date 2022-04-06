5b478daeb5f3d38abcca0920b2bf4d6d7e13643c
samisassi8 <samibrahimmac@gmail.com>
2022-04-06 15:37:51 +0200

    project ended

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
 import { Link } from "react-router-dom";
+<<<<<<< HEAD
 import "./home.css";
 import Slideshow from "./slideshow/slideshow";
+=======
+>>>>>>> 18f3b85... project ended
 
 export default function Home() {
   return (
     <div className="home__container">
       <Fade top cascade>
         <Link to="/services">
+<<<<<<< HEAD
           <h1 className="home__title">JARDIMOTORS</h1>
         </Link>
         <Slideshow />
@@ -20,6 +24,10 @@ export default function Home() {
           <li>Matériel de motoculture</li>
           <li>TOUTES MARQUES</li>
         </ul>
+=======
+          <h1>JARDIMOTORS</h1>
+        </Link>
+>>>>>>> 18f3b85... project ended
       </Fade>
     </div>
   );
