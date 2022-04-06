464ea4fbdb83a81992ae920a8d35d06a38cf2bd6
samisassi8 <samibrahimmac@gmail.com>
2022-04-06 15:37:51 +0200

    caroussel in progress

--- src/components/slideshow/slideshow.js
+++ src/components/slideshow/slideshow.js
@@ -1,53 +1,48 @@
 import React from "react";
 import { Slide } from "react-slideshow-image";
+import img1 from "../../assets/img/img1.jpg";
+import img2 from "../../assets/img/img2.jpg";
+import img3 from "../../assets/img/img3.jpg";
+import img4 from "../../assets/img/img4.jpg";
+import img5 from "../../assets/img/img5.jpg";
 import "./slideshow.css";
+import "react-slideshow-image/dist/styles.css";
 
-const slideImages = [
-  "../../assets/img/img1.jpg",
-  "../../assets/img/img2.jpg",
-  "../../assets/img/img3.jpg",
-  "../../assets/img/img4.jpg",
-  "../../assets/img/img5.jpg",
-];
-
-const properties = {
+const proprietes = {
   duration: 5000,
   transitionDuration: 500,
   infinite: true,
   indicators: true,
   arrows: true,
-  onChange: (oldIndex, newIndex) => {
-    console.log(`slide transition from ${oldIndex} to ${newIndex}`);
-  },
 };
 
 const Slideshow = () => {
   return (
-    <div className="slide-container">
-      <Slide {...properties}>
+    <div className="containerSlide">
+      <Slide {...proprietes}>
         <div className="each-slide">
-          <div style={{ backgroundImage: `url(${slideImages[0]})` }}>
-            <span>Slide 1</span>
+          <div>
+            <img src={img1} alt="img1" />
           </div>
         </div>
         <div className="each-slide">
-          <div style={{ backgroundImage: `url(${slideImages[1]})` }}>
-            <span>Slide 2</span>
+          <div>
+            <img src={img2} alt="img2" />
           </div>
         </div>
         <div className="each-slide">
-          <div style={{ backgroundImage: `url(${slideImages[2]})` }}>
-            <span>Slide 3</span>
+          <div>
+            <img src={img3} alt="img3" />
           </div>
         </div>
         <div className="each-slide">
-          <div style={{ backgroundImage: `url(${slideImages[3]})` }}>
-            <span>Slide 4</span>
+          <div>
+            <img src={img4} alt="img4" />
           </div>
         </div>
         <div className="each-slide">
-          <div style={{ backgroundImage: `url(${slideImages[4]})` }}>
-            <span>Slide 5</span>
+          <div>
+            <img src={img5} alt="img5" />
           </div>
         </div>
       </Slide>
